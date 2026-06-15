df8cee055aff1772c28cad81fcf81e711034479c
Sujip Thapa <sudiptpa@gmail.com>
2026-06-15 07:24:20 +0545

    fix: Accounting Invoice exposes Date, DueDate, LineAmountTypes, InvoiceNumber, BrandingThemeID, Url, CurrencyCode, CurrencyRate, SentToContact, ExpectedPaymentDate, PlannedPaymentDate, CISDeduction, CISRate, SubTotal, TotalTax, Total, TotalDiscount, RepeatingInvoiceID, HasAttachments, IsDiscounted, Payments, Prepayments, Overpayments, AmountDue, AmountPaid, FullyPaidOnDate, AmountCredited, UpdatedDateUTC, CreditNotes, Attachments, HasErrors, StatusAttributeString, ValidationErrors, Warnings, InvoiceAddresses

--- src/Accounting/Invoice/Invoice.php
+++ src/Accounting/Invoice/Invoice.php
@@ -6,9 +6,16 @@ namespace Sujip\Xero\Accounting\Invoice;
 
 use RuntimeException;
 use Sujip\Xero\Accounting\Contact\Contact;
+use Sujip\Xero\Accounting\CreditNote\CreditNote;
+use Sujip\Xero\Accounting\Overpayment\Overpayment;
+use Sujip\Xero\Accounting\Payment\Payment;
+use Sujip\Xero\Accounting\Prepayment\Prepayment;
 use Sujip\Xero\Client;
+use Sujip\Xero\Support\AttachmentDetail;
 use Sujip\Xero\Support\Field;
+use Sujip\Xero\Support\InvoiceAddress;
 use Sujip\Xero\Support\Model;
+use Sujip\Xero\Support\ValidationError;
 use Sujip\Xero\Support\Contracts\SerializesRequest;
 
 final class Invoice extends Model implements SerializesRequest
@@ -27,6 +34,100 @@ final class Invoice extends Model implements SerializesRequest
      * @var list<LineItem>
      */
     private array $lineItems = [];
+
+    private ?string $date = null;
+
+    private ?string $dueDate = null;
+
+    private ?string $lineAmountTypes = null;
+
+    private ?string $invoiceNumber = null;
+
+    private ?string $brandingThemeID = null;
+
+    private ?string $url = null;
+
+    private ?string $currencyCode = null;
+
+    private int|float|null $currencyRate = null;
+
+    private ?bool $sentToContact = null;
+
+    private ?string $expectedPaymentDate = null;
+
+    private ?string $plannedPaymentDate = null;
+
+    private int|float|null $cisDeduction = null;
+
+    private int|float|null $cisRate = null;
+
+    private int|float|null $subTotal = null;
+
+    private int|float|null $totalTax = null;
+
+    private int|float|null $total = null;
+
+    private int|float|null $totalDiscount = null;
+
+    private ?string $repeatingInvoiceID = null;
+
+    private ?bool $hasAttachments = null;
+
+    private ?bool $isDiscounted = null;
+
+    /**
+     * @var list<Payment>
+     */
+    private array $payments = [];
+
+    /**
+     * @var list<Prepayment>
+     */
+    private array $prepayments = [];
+
+    /**
+     * @var list<Overpayment>
+     */
+    private array $overpayments = [];
+
+    private int|float|null $amountDue = null;
+
+    private int|float|null $amountPaid = null;
+
+    private ?string $fullyPaidOnDate = null;
+
+    private int|float|null $amountCredited = null;
+
+    private ?string $updatedDateUTC = null;
+
+    /**
+     * @var list<CreditNote>
+     */
+    private array $creditNotes = [];
+
+    /**
+     * @var list<AttachmentDetail>
+     */
+    private array $attachments = [];
+
+    private ?bool $hasErrors = null;
+
+    private ?string $statusAttributeString = null;
+
+    /**
+     * @var list<ValidationError>
+     */
+    private array $validationErrors = [];
+
+    /**
+     * @var list<ValidationError>
+     */
+    private array $warnings = [];
+
+    /**
+     * @var list<InvoiceAddress>
+     */
+    private array $invoiceAddresses = [];
 
     public function __construct(
         private ?Client $client = null
@@ -132,6 +233,450 @@ final class Invoice extends Model implements SerializesRequest
         return $this;
     }
 
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setDate(?string $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?string
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(?string $dueDate): self
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    public function getLineAmountTypes(): ?string
+    {
+        return $this->lineAmountTypes;
+    }
+
+    public function setLineAmountTypes(?string $lineAmountTypes): self
+    {
+        $this->lineAmountTypes = $lineAmountTypes;
+
+        return $this;
+    }
+
+    public function getInvoiceNumber(): ?string
+    {
+        return $this->invoiceNumber;
+    }
+
+    public function setInvoiceNumber(?string $invoiceNumber): self
+    {
+        $this->invoiceNumber = $invoiceNumber;
+
+        return $this;
+    }
+
+    public function getBrandingThemeID(): ?string
+    {
+        return $this->brandingThemeID;
+    }
+
+    public function setBrandingThemeID(?string $brandingThemeID): self
+    {
+        $this->brandingThemeID = $brandingThemeID;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getCurrencyCode(): ?string
+    {
+        return $this->currencyCode;
+    }
+
+    public function setCurrencyCode(?string $currencyCode): self
+    {
+        $this->currencyCode = $currencyCode;
+
+        return $this;
+    }
+
+    public function getCurrencyRate(): int|float|null
+    {
+        return $this->currencyRate;
+    }
+
+    public function setCurrencyRate(int|float|null $currencyRate): self
+    {
+        $this->currencyRate = $currencyRate;
+
+        return $this;
+    }
+
+    public function getSentToContact(): ?bool
+    {
+        return $this->sentToContact;
+    }
+
+    public function setSentToContact(?bool $sentToContact): self
+    {
+        $this->sentToContact = $sentToContact;
+
+        return $this;
+    }
+
+    public function getExpectedPaymentDate(): ?string
+    {
+        return $this->expectedPaymentDate;
+    }
+
+    public function setExpectedPaymentDate(?string $expectedPaymentDate): self
+    {
+        $this->expectedPaymentDate = $expectedPaymentDate;
+
+        return $this;
+    }
+
+    public function getPlannedPaymentDate(): ?string
+    {
+        return $this->plannedPaymentDate;
+    }
+
+    public function setPlannedPaymentDate(?string $plannedPaymentDate): self
+    {
+        $this->plannedPaymentDate = $plannedPaymentDate;
+
+        return $this;
+    }
+
+    public function getCISDeduction(): int|float|null
+    {
+        return $this->cisDeduction;
+    }
+
+    public function setCISDeduction(int|float|null $cisDeduction): self
+    {
+        $this->cisDeduction = $cisDeduction;
+
+        return $this;
+    }
+
+    public function getCISRate(): int|float|null
+    {
+        return $this->cisRate;
+    }
+
+    public function setCISRate(int|float|null $cisRate): self
+    {
+        $this->cisRate = $cisRate;
+
+        return $this;
+    }
+
+    public function getSubTotal(): int|float|null
+    {
+        return $this->subTotal;
+    }
+
+    public function setSubTotal(int|float|null $subTotal): self
+    {
+        $this->subTotal = $subTotal;
+
+        return $this;
+    }
+
+    public function getTotalTax(): int|float|null
+    {
+        return $this->totalTax;
+    }
+
+    public function setTotalTax(int|float|null $totalTax): self
+    {
+        $this->totalTax = $totalTax;
+
+        return $this;
+    }
+
+    public function getTotal(): int|float|null
+    {
+        return $this->total;
+    }
+
+    public function setTotal(int|float|null $total): self
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+
+    public function getTotalDiscount(): int|float|null
+    {
+        return $this->totalDiscount;
+    }
+
+    public function setTotalDiscount(int|float|null $totalDiscount): self
+    {
+        $this->totalDiscount = $totalDiscount;
+
+        return $this;
+    }
+
+    public function getRepeatingInvoiceID(): ?string
+    {
+        return $this->repeatingInvoiceID;
+    }
+
+    public function setRepeatingInvoiceID(?string $repeatingInvoiceID): self
+    {
+        $this->repeatingInvoiceID = $repeatingInvoiceID;
+
+        return $this;
+    }
+
+    public function getHasAttachments(): ?bool
+    {
+        return $this->hasAttachments;
+    }
+
+    public function setHasAttachments(?bool $hasAttachments): self
+    {
+        $this->hasAttachments = $hasAttachments;
+
+        return $this;
+    }
+
+    public function getIsDiscounted(): ?bool
+    {
+        return $this->isDiscounted;
+    }
+
+    public function setIsDiscounted(?bool $isDiscounted): self
+    {
+        $this->isDiscounted = $isDiscounted;
+
+        return $this;
+    }
+
+    /**
+     * @return list<Payment>
+     */
+    public function getPayments(): array
+    {
+        return $this->payments;
+    }
+
+    public function addPayment(Payment $payment): self
+    {
+        $this->payments[] = $payment;
+
+        return $this;
+    }
+
+    /**
+     * @return list<Prepayment>
+     */
+    public function getPrepayments(): array
+    {
+        return $this->prepayments;
+    }
+
+    public function addPrepayment(Prepayment $prepayment): self
+    {
+        $this->prepayments[] = $prepayment;
+
+        return $this;
+    }
+
+    /**
+     * @return list<Overpayment>
+     */
+    public function getOverpayments(): array
+    {
+        return $this->overpayments;
+    }
+
+    public function addOverpayment(Overpayment $overpayment): self
+    {
+        $this->overpayments[] = $overpayment;
+
+        return $this;
+    }
+
+    public function getAmountDue(): int|float|null
+    {
+        return $this->amountDue;
+    }
+
+    public function setAmountDue(int|float|null $amountDue): self
+    {
+        $this->amountDue = $amountDue;
+
+        return $this;
+    }
+
+    public function getAmountPaid(): int|float|null
+    {
+        return $this->amountPaid;
+    }
+
+    public function setAmountPaid(int|float|null $amountPaid): self
+    {
+        $this->amountPaid = $amountPaid;
+
+        return $this;
+    }
+
+    public function getFullyPaidOnDate(): ?string
+    {
+        return $this->fullyPaidOnDate;
+    }
+
+    public function setFullyPaidOnDate(?string $fullyPaidOnDate): self
+    {
+        $this->fullyPaidOnDate = $fullyPaidOnDate;
+
+        return $this;
+    }
+
+    public function getAmountCredited(): int|float|null
+    {
+        return $this->amountCredited;
+    }
+
+    public function setAmountCredited(int|float|null $amountCredited): self
+    {
+        $this->amountCredited = $amountCredited;
+
+        return $this;
+    }
+
+    public function getUpdatedDateUTC(): ?string
+    {
+        return $this->updatedDateUTC;
+    }
+
+    public function setUpdatedDateUTC(?string $updatedDateUTC): self
+    {
+        $this->updatedDateUTC = $updatedDateUTC;
+
+        return $this;
+    }
+
+    /**
+     * @return list<CreditNote>
+     */
+    public function getCreditNotes(): array
+    {
+        return $this->creditNotes;
+    }
+
+    public function addCreditNote(CreditNote $creditNote): self
+    {
+        $this->creditNotes[] = $creditNote;
+
+        return $this;
+    }
+
+    /**
+     * @return list<AttachmentDetail>
+     */
+    public function getAttachments(): array
+    {
+        return $this->attachments;
+    }
+
+    public function addAttachment(AttachmentDetail $attachment): self
+    {
+        $this->attachments[] = $attachment;
+
+        return $this;
+    }
+
+    public function getHasErrors(): ?bool
+    {
+        return $this->hasErrors;
+    }
+
+    public function setHasErrors(?bool $hasErrors): self
+    {
+        $this->hasErrors = $hasErrors;
+
+        return $this;
+    }
+
+    public function getStatusAttributeString(): ?string
+    {
+        return $this->statusAttributeString;
+    }
+
+    public function setStatusAttributeString(?string $statusAttributeString): self
+    {
+        $this->statusAttributeString = $statusAttributeString;
+
+        return $this;
+    }
+
+    /**
+     * @return list<ValidationError>
+     */
+    public function getValidationErrors(): array
+    {
+        return $this->validationErrors;
+    }
+
+    public function addValidationError(ValidationError $validationError): self
+    {
+        $this->validationErrors[] = $validationError;
+
+        return $this;
+    }
+
+    /**
+     * @return list<ValidationError>
+     */
+    public function getWarnings(): array
+    {
+        return $this->warnings;
+    }
+
+    public function addWarning(ValidationError $warning): self
+    {
+        $this->warnings[] = $warning;
+
+        return $this;
+    }
+
+    /**
+     * @return list<InvoiceAddress>
+     */
+    public function getInvoiceAddresses(): array
+    {
+        return $this->invoiceAddresses;
+    }
+
+    public function addInvoiceAddress(InvoiceAddress $invoiceAddress): self
+    {
+        $this->invoiceAddresses[] = $invoiceAddress;
+
+        return $this;
+    }
+
     /**
      * @return array<string, Field>
      */
@@ -144,6 +689,41 @@ final class Invoice extends Model implements SerializesRequest
             'Type' => Field::string(),
             'Contact' => Field::object(Contact::class),
             'LineItems' => Field::many(LineItem::class),
+            'Date' => Field::string(),
+            'DueDate' => Field::string(),
+            'LineAmountTypes' => Field::string(),
+            'InvoiceNumber' => Field::string(),
+            'BrandingThemeID' => Field::string(),
+            'Url' => Field::string(),
+            'CurrencyCode' => Field::string(),
+            'CurrencyRate' => Field::number(),
+            'SentToContact' => Field::boolean(),
+            'ExpectedPaymentDate' => Field::string(),
+            'PlannedPaymentDate' => Field::string(),
+            'CISDeduction' => Field::number(),
+            'CISRate' => Field::number(),
+            'SubTotal' => Field::number(),
+            'TotalTax' => Field::number(),
+            'Total' => Field::number(),
+            'TotalDiscount' => Field::number(),
+            'RepeatingInvoiceID' => Field::string(),
+            'HasAttachments' => Field::boolean(),
+            'IsDiscounted' => Field::boolean(),
+            'Payments' => Field::many(Payment::class),
+            'Prepayments' => Field::many(Prepayment::class),
+            'Overpayments' => Field::many(Overpayment::class),
+            'AmountDue' => Field::number(),
+            'AmountPaid' => Field::number(),
+            'FullyPaidOnDate' => Field::string(),
+            'AmountCredited' => Field::number(),
+            'UpdatedDateUTC' => Field::string(),
+            'CreditNotes' => Field::many(CreditNote::class),
+            'Attachments' => Field::many(AttachmentDetail::class),
+            'HasErrors' => Field::boolean(),
+            'StatusAttributeString' => Field::string(),
+            'ValidationErrors' => Field::many(ValidationError::class),
+            'Warnings' => Field::many(ValidationError::class),
+            'InvoiceAddresses' => Field::many(InvoiceAddress::class),
         ];
     }
 
@@ -151,6 +731,14 @@ final class Invoice extends Model implements SerializesRequest
     {
         if ($class === Contact::class) {
             return new Contact($this->client);
+        }
+
+        if ($class === Payment::class) {
+            return new Payment($this->client);
+        }
+
+        if ($class === CreditNote::class) {
+            return new CreditNote($this->client);
         }
 
         return parent::newDefinitionInstance($class);
@@ -172,6 +760,22 @@ final class Invoice extends Model implements SerializesRequest
             'LineItems' => array_map(
                 static fn (LineItem $lineItem): array => $lineItem->toRequest(),
                 $this->getLineItems()
+            ),
+            'Date' => $this->getDate(),
+            'DueDate' => $this->getDueDate(),
+            'LineAmountTypes' => $this->getLineAmountTypes(),
+            'InvoiceNumber' => $this->getInvoiceNumber(),
+            'BrandingThemeID' => $this->getBrandingThemeID(),
+            'Url' => $this->getUrl(),
+            'CurrencyCode' => $this->getCurrencyCode(),
+            'CurrencyRate' => $this->getCurrencyRate(),
+            'SentToContact' => $this->getSentToContact(),
+            'ExpectedPaymentDate' => $this->getExpectedPaymentDate(),
+            'PlannedPaymentDate' => $this->getPlannedPaymentDate(),
+            'RepeatingInvoiceID' => $this->getRepeatingInvoiceID(),
+            'InvoiceAddresses' => array_map(
+                static fn (InvoiceAddress $invoiceAddress): array => $invoiceAddress->toRequest(),
+                $this->getInvoiceAddresses()
             ),
         ], static fn (mixed $value): bool => $value !== null);
     }
--- tests/Accounting/InvoicesTest.php
+++ tests/Accounting/InvoicesTest.php
@@ -123,6 +123,41 @@ final class InvoicesTest extends TestCase
                     'Status' => 'PAID',
                     'Reference' => 'BILL-1001',
                     'LineItems' => [],
+                    'Date' => '2026-01-01T00:00:00',
+                    'DueDate' => '2026-01-31T00:00:00',
+                    'LineAmountTypes' => 'Exclusive',
+                    'InvoiceNumber' => 'INV-0001',
+                    'BrandingThemeID' => 'theme-1',
+                    'Url' => 'https://example.test/invoice',
+                    'CurrencyCode' => 'NZD',
+                    'CurrencyRate' => 1.5,
+                    'SentToContact' => true,
+                    'ExpectedPaymentDate' => '2026-02-01T00:00:00',
+                    'PlannedPaymentDate' => '2026-02-02T00:00:00',
+                    'CISDeduction' => 10.5,
+                    'CISRate' => 0.25,
+                    'SubTotal' => 100.5,
+                    'TotalTax' => 15.25,
+                    'Total' => 115.75,
+                    'TotalDiscount' => 5.25,
+                    'RepeatingInvoiceID' => 'repeating-1',
+                    'HasAttachments' => true,
+                    'IsDiscounted' => true,
+                    'Payments' => [['PaymentID' => 'payment-1']],
+                    'Prepayments' => [['PrepaymentID' => 'prepayment-1']],
+                    'Overpayments' => [['OverpaymentID' => 'overpayment-1']],
+                    'AmountDue' => 50.25,
+                    'AmountPaid' => 65.75,
+                    'FullyPaidOnDate' => '2026-02-03T00:00:00',
+                    'AmountCredited' => 0.01,
+                    'UpdatedDateUTC' => '2026-01-02T00:00:00',
+                    'CreditNotes' => [['CreditNoteID' => 'credit-note-1']],
+                    'Attachments' => [['AttachmentID' => 'attachment-1', 'FileName' => 'receipt.pdf']],
+                    'HasErrors' => false,
+                    'StatusAttributeString' => 'OK',
+                    'ValidationErrors' => [['Message' => 'something went wrong']],
+                    'Warnings' => [['Message' => 'a warning']],
+                    'InvoiceAddresses' => [['InvoiceAddressType' => 'POBOX', 'City' => 'Auckland']],
                 ]],
             ], JSON_THROW_ON_ERROR))
         );
@@ -136,6 +171,67 @@ final class InvoicesTest extends TestCase
         self::assertNotNull($invoice);
         self::assertSame('invoice-1', $invoice->getInvoiceID());
         self::assertSame('ACCPAY', $invoice->getType());
+        self::assertSame('2026-01-01T00:00:00', $invoice->getDate());
+        self::assertSame('2026-01-31T00:00:00', $invoice->getDueDate());
+        self::assertSame('Exclusive', $invoice->getLineAmountTypes());
+        self::assertSame('INV-0001', $invoice->getInvoiceNumber());
+        self::assertSame('theme-1', $invoice->getBrandingThemeID());
+        self::assertSame('https://example.test/invoice', $invoice->getUrl());
+        self::assertSame('NZD', $invoice->getCurrencyCode());
+        self::assertSame(1.5, $invoice->getCurrencyRate());
+        self::assertTrue($invoice->getSentToContact());
+        self::assertSame('2026-02-01T00:00:00', $invoice->getExpectedPaymentDate());
+        self::assertSame('2026-02-02T00:00:00', $invoice->getPlannedPaymentDate());
+        self::assertSame(10.5, $invoice->getCISDeduction());
+        self::assertSame(0.25, $invoice->getCISRate());
+        self::assertSame(100.5, $invoice->getSubTotal());
+        self::assertSame(15.25, $invoice->getTotalTax());
+        self::assertSame(115.75, $invoice->getTotal());
+        self::assertSame(5.25, $invoice->getTotalDiscount());
+        self::assertSame('repeating-1', $invoice->getRepeatingInvoiceID());
+        self::assertTrue($invoice->getHasAttachments());
+        self::assertTrue($invoice->getIsDiscounted());
+        self::assertSame(50.25, $invoice->getAmountDue());
+        self::assertSame(65.75, $invoice->getAmountPaid());
+        self::assertSame('2026-02-03T00:00:00', $invoice->getFullyPaidOnDate());
+        self::assertSame(0.01, $invoice->getAmountCredited());
+        self::assertSame('2026-01-02T00:00:00', $invoice->getUpdatedDateUTC());
+        self::assertFalse($invoice->getHasErrors());
+        self::assertSame('OK', $invoice->getStatusAttributeString());
+
+        $payments = $invoice->getPayments();
+        self::assertCount(1, $payments);
+        self::assertSame('payment-1', $payments[0]->getPaymentID());
+
+        $prepayments = $invoice->getPrepayments();
+        self::assertCount(1, $prepayments);
+        self::assertSame('prepayment-1', $prepayments[0]->getPrepaymentID());
+
+        $overpayments = $invoice->getOverpayments();
+        self::assertCount(1, $overpayments);
+        self::assertSame('overpayment-1', $overpayments[0]->getOverpaymentID());
+
+        $creditNotes = $invoice->getCreditNotes();
+        self::assertCount(1, $creditNotes);
+        self::assertSame('credit-note-1', $creditNotes[0]->getCreditNoteID());
+
+        $attachments = $invoice->getAttachments();
+        self::assertCount(1, $attachments);
+        self::assertSame('attachment-1', $attachments[0]->getAttachmentID());
+        self::assertSame('receipt.pdf', $attachments[0]->getFileName());
+
+        $validationErrors = $invoice->getValidationErrors();
+        self::assertCount(1, $validationErrors);
+        self::assertSame('something went wrong', $validationErrors[0]->getMessage());
+
+        $warnings = $invoice->getWarnings();
+        self::assertCount(1, $warnings);
+        self::assertSame('a warning', $warnings[0]->getMessage());
+
+        $invoiceAddresses = $invoice->getInvoiceAddresses();
+        self::assertCount(1, $invoiceAddresses);
+        self::assertSame('POBOX', $invoiceAddresses[0]->getInvoiceAddressType());
+        self::assertSame('Auckland', $invoiceAddresses[0]->getCity());
     }
 
     public function test_it_can_update_an_invoice(): void
